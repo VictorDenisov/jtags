ba51a80cbf478cf28d7be559992b20935aa0107c
Victor Denisov <victor@victor-laptop.(none)>
2010-05-28 09:10:26 +0400

    Support of methods and fields is added.

--- src/main/java/CodeVisitor.java
+++ src/main/java/CodeVisitor.java
@@ -1,6 +1,7 @@
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.body.ConstructorDeclaration;
+import japa.parser.ast.body.VariableDeclarator;
 import japa.parser.ast.expr.VariableDeclarationExpr;
 import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.expr.MethodCallExpr;
@@ -17,21 +18,36 @@ public class CodeVisitor extends VoidVisitorAdapter<String> {
 
     @Override
     public void visit(FieldDeclaration n, String file) {
+        for (VariableDeclarator var : n.getVariables()) {
+            tags.addElement(new Tag(var.getId().getName(), file, var.getId().getBeginLine() + ""));
+        }
+        
+        super.visit(n, file);
     }
 
     @Override
     public void visit(ConstructorDeclaration n, String file) {
+        super.visit(n, file);
     }
 
     @Override
     public void visit(VariableDeclarationExpr n, String file) {
+        super.visit(n, file);
+    }
+
+    @Override
+    public void visit(VariableDeclarator n, String file) {
+        super.visit(n, file);
     }
 
     @Override
     public void visit(MethodDeclaration n, String file) {
+        tags.addElement(new Tag(n.getName(), file, n.getBeginLine() + ""));
+        super.visit(n, file);
     }
 
     @Override
     public void visit(MethodCallExpr n, String file) {
+        super.visit(n, file);
     }
 }
